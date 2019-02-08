fdf3534e08fad4c696d47173f192739adb7bdc1d
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2019-02-07 20:32:16 -0600

    WIP

--- packages/embark-compiler/src/index.ts
+++ packages/embark-compiler/src/index.ts
@@ -1,5 +1,6 @@
 import {Callback, CompilerPluginObject, Embark, Plugins} from "embark";
 import {__} from "i18n";
+import {promisify} from "util";
 
 const async = require("./utils/async_extend.js");
 
@@ -27,44 +28,57 @@ class Compiler {
       isCoverage: this.isCoverage,
     };
 
-    async.eachObject(this.getAvailableCompilers(),
-      (extension: string, compilers: any, next: any) => {
-        const matchingFiles = contractFiles.filter(this.filesMatchingExtension(extension));
-        if (matchingFiles.length === 0) {
-          return next();
-        }
+    (async () => {
+      let err = null;
+      try {
+        await Promise.all(
+          // parallel exec in map
+          Object.entries(this.getAvailableCompilers()).map(
+            async ([extension, compilers]) => {
+              const matchingFiles = contractFiles.filter(
+                this.filesMatchingExtension(extension)
+              );
+              if (matchingFiles.length === 0) { return; }
 
-        async.someLimit(compilers, 1, (compiler: any, someCb: Callback<boolean>) => {
-          compiler.call(compiler, matchingFiles, compilerOptions, (err: any, compileResult: any) => {
-            if (err) {
-              return someCb(err);
+              const runCompilers = function*() {
+                for (const compiler of compilers) {
+                  yield promisify(compiler)(matchingFiles, compilerOptions);
+                }
+              };
+
+              let result;
+              // serial exec in for-await...of
+              for await (result of runCompilers()) {
+                if (result !== false) { break; }
+              }
+
+              if (!result) {
+                // No compiler was compatible
+                throw new Error(__([
+                  `No installed compiler was compatible with your version of`,
+                  `${extension} files.`
+                ].join(' ')))
+              }
+
+              Object.assign(compiledObject, result);
             }
-            if (compileResult === false) {
-              // Compiler not compatible, trying the next one
-              return someCb(null, false);
-            }
-            Object.assign(compiledObject, compileResult);
-            someCb(null, true);
-          });
-        }, (err: Error, result: boolean) => {
-          if (err) {
-            return next(err);
+          )
+        );
+      } catch (e) {
+        err = e;
+      } finally {
+        contractFiles.filter((f: any) => !f.compiled).forEach(
+          (file: any) => {
+            this.logger.warn(__([
+              `${file.path} doesn't have a compatible contract compiler.`,
+              `Maybe a plugin exists for it.`
+            ].join(' ')));
           }
-          if (!result) {
-            // No compiler was compatible
-            return next(new Error(__("No installed compiler was compatible with your version of %s files", extension)));
-          }
-          next();
-        });
-      },
-      (err: any) => {
-        contractFiles.filter((f: any) => !f.compiled).forEach((file: any) => {
-          this.logger.warn(__("%s doesn't have a compatible contract compiler. Maybe a plugin exists for it.", file.path));
-        });
+        );
 
         cb(err, compiledObject);
-      },
-    );
+      }
+    })();
   }
 
   private getAvailableCompilers() {
